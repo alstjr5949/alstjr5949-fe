74993a4c675937dedc273e1beff04b4257612026
alstjr5949 <alstjr5949@gmail.com>
2022-11-13 01:00:07 +0900

    <feat> 무한 스크롤 구현

--- src/pages/infinite-scroll.tsx
+++ src/pages/infinite-scroll.tsx
@@ -1,18 +1,48 @@
-import Link from 'next/link';
 import type { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
-import products from '../api/data/products.json';
 import ProductList from '../components/ProductList';
 import Header from '../components/Header';
+import axios from 'axios';
+import { useInfiniteQuery } from 'react-query';
+import { Product } from '../types/product';
+import { useInView } from 'react-intersection-observer';
 
 const InfiniteScrollPage: NextPage = () => {
+  const { ref, inView } = useInView({ threshold: 0.3 });
+
+  const getInfProductsData = async (page: number) => {
+    const res = await axios.get(`/products?page=1&size=${page}`);
+    return res.data.data.products;
+  };
+
+  const { data, status, fetchNextPage, hasNextPage } = useInfiniteQuery<Product[] | undefined>(
+    'products',
+    async ({ pageParam = 10 }) => {
+      return await getInfProductsData(pageParam);
+    },
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const maxPage = 11;
+        const nextPage = allPages.length + 1;
+        return nextPage <= maxPage ? nextPage * 10 : undefined;
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
   return (
     <>
       <Header />
       <Container>
-        <ProductList products={products} />
+        {status === 'success' && <ProductList products={data.pages[data?.pages.length - 1]} />}
+        <div ref={ref}></div>
       </Container>
     </>
   );
